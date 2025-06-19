45591129b90739e548915b78aadea916534d92e9
MidnightMagenta <antekswiergocki@gmail.com>
2025-06-19 02:26:58 +0200

    Adds physical memory allocation

--- src/kernel/include/memory/pmm.hpp
+++ src/kernel/include/memory/pmm.hpp
@@ -8,12 +8,21 @@
 namespace MdOS::Memory {
 class PhysicalMemoryManager {
 public:
+	struct Allocation{
+		uintptr_t base;
+		size_t numPages;
+	};
+
 	static void init(MemMap *memMap);
 
+	static Allocation allocPage();
+	static Allocation allocPage(size_t numPages);
+	static void freePage(Allocation alloc);
 private:
 	static bool m_initialized;
 	static size_t m_maxAvailPages;
 	static size_t m_freePageCount;
+	static size_t m_usedPageCount;
 	static utils::Bitmap<uint32_t> m_pageFrameMap;
 };
 }// namespace MdOS::Memory
--- src/kernel/src/memory/pmm.cpp
+++ src/kernel/src/memory/pmm.cpp
@@ -3,13 +3,15 @@
 
 size_t MdOS::Memory::PhysicalMemoryManager::m_maxAvailPages = 0;
 size_t MdOS::Memory::PhysicalMemoryManager::m_freePageCount = 0;
+size_t MdOS::Memory::PhysicalMemoryManager::m_usedPageCount = 0;
 utils::Bitmap<uint32_t> MdOS::Memory::PhysicalMemoryManager::m_pageFrameMap = utils::Bitmap<uint32_t>();
 bool MdOS::Memory::PhysicalMemoryManager::m_initialized = false;
 
 void MdOS::Memory::PhysicalMemoryManager::init(MemMap *memMap) {
 	if (m_initialized) { return; }
+	m_initialized = true;
 
-	uintptr_t lowestAddr = 0;
+	uintptr_t lowestAddr = UINTPTR_MAX;
 	uintptr_t highestAddr = 0;
 
 	for (EFI_MEMORY_DESCRIPTOR *entry = memMap->map;
@@ -23,7 +25,7 @@ void MdOS::Memory::PhysicalMemoryManager::init(MemMap *memMap) {
 
 	m_maxAvailPages = (highestAddr - lowestAddr) / 0x1000;
 
-	if (!m_pageFrameMap.init(m_maxAvailPages, true)) {
+	if (!m_pageFrameMap.init(m_maxAvailPages, true) /*all pages marked as used*/) {
 		MdOS::IO::kprint("Failed to initialize page frame map\n");
 		//TODO: panic
 	}
@@ -32,14 +34,65 @@ void MdOS::Memory::PhysicalMemoryManager::init(MemMap *memMap) {
 		 entry < (EFI_MEMORY_DESCRIPTOR *) (uintptr_t(memMap->map) + uintptr_t(memMap->size));
 		 entry = (EFI_MEMORY_DESCRIPTOR *) (uintptr_t(entry) + (memMap->descriptorSize))) {
 		if ((entry->type == EfiConventionalMemory) || (entry->type == EfiBootServicesCode) ||
-			(entry->type == EfiBootServicesData)) {
+			(entry->type == EfiBootServicesData) || (entry->type == EfiLoaderCode)) {
 			m_freePageCount += entry->pageCount;
 			m_pageFrameMap.clear_range(entry->paddr / 0x1000, (entry->paddr / 0x1000) + entry->pageCount);
 		}
 	}
+}
 
-	MdOS::IO::kprint("Memory attributes:\n\tLowest addr: 0x%lx\n\tHighest addr: 0x%lx\n\tPage count: %lu\n\tFree "
-					 "pages: %lu\n\tTotal memory: %lu MiB\n\tAvailable memory: %lu MiB\n\n",
-					 lowestAddr, highestAddr, m_maxAvailPages, m_freePageCount,
-					 (m_maxAvailPages * 0x1000) / 1024 / 1024, (m_freePageCount * 0x1000) / 1024 / 1024);
+MdOS::Memory::PhysicalMemoryManager::Allocation MdOS::Memory::PhysicalMemoryManager::allocPage() {
+	Allocation res{0, 0};
+	if (m_freePageCount == 0) {
+		//TODO: Panic
+		MdOS::IO::kprint("Out of memory");
+		return res;
+	}
+	m_freePageCount -= 1;
+	m_usedPageCount += 1;
+	size_t firstFreePage = m_pageFrameMap.find_first_clear_bit();
+	m_pageFrameMap.set(firstFreePage);
+	res.numPages = 1;
+	res.base = firstFreePage * 0x1000;
+	return res;
+}
+
+MdOS::Memory::PhysicalMemoryManager::Allocation MdOS::Memory::PhysicalMemoryManager::allocPage(size_t numPages) {
+	Allocation res{0, 0};
+	if (m_freePageCount < numPages) { return res; }
+
+	size_t lastFreeIndex = m_pageFrameMap.find_first_clear_bit();
+	while (lastFreeIndex < m_pageFrameMap.size()) {
+		if (lastFreeIndex + numPages >= m_pageFrameMap.size()) { break; }
+		bool rangeContinous = true;
+		for (size_t i = 0; i < numPages; i++) {
+			if (m_pageFrameMap[lastFreeIndex + i]) {
+				rangeContinous = false;
+				lastFreeIndex = m_pageFrameMap.find_next_clear_bit(lastFreeIndex + i + 1);
+				break;
+			}
+		}
+		if (rangeContinous) {
+			for (size_t i = 0; i < numPages; i++) { m_pageFrameMap.set(lastFreeIndex + i); }
+			m_freePageCount -= numPages;
+			m_usedPageCount += numPages;
+
+			res.base = lastFreeIndex * 0x1000;
+			res.numPages = numPages;
+			break;
+		}
+	}
+	return res;
+}
+
+void MdOS::Memory::PhysicalMemoryManager::freePage(Allocation alloc) {
+	if ((alloc.base % 0x1000) != 0) {
+		//TODO: Panic
+		MdOS::IO::kprint("Attempted to free a page with a non page aligned address");
+		return;
+	}
+	if (!m_pageFrameMap[alloc.base / 0x1000]) { return; }
+	m_freePageCount += alloc.numPages;
+	m_usedPageCount -= alloc.numPages;
+	for (size_t i = alloc.base / 0x1000; i < alloc.numPages + alloc.base / 0x1000; i++) { m_pageFrameMap.clear(i); }
 }
