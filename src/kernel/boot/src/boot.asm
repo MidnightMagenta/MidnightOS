aecc3932c3e6393deedea1672e781af64094dc85
MidnightMagenta <antekswiergocki@gmail.com>
2025-06-24 00:15:56 +0200

    Changes bootInfo storing logic in boot.asm

--- src/kernel/boot/src/boot.asm
+++ src/kernel/boot/src/boot.asm
@@ -8,20 +8,17 @@ global _start
 
 section .bss
 	align 16
-	_stack_bottom:	 			; kernel's bootstrap stack
+	_stack_bottom:				; kernel's bootstrap stack
 		resb 0x4000
 	_stack_top:
 
-section .data
-	_bootInfo: dq 0x0  			; temporary storage for bootInfo pointer while _init is being called
-
 section .text
 	_start:
-		cli                     ; mask all interupts
-		mov rsp, _stack_top     ; set stack pointer to the kernel's stack
-		mov [_bootInfo], rdi    ; store bootInfo prior to calling _init
+		cli						; mask all interupts
+		mov rsp, _stack_top		; set stack pointer to the kernel's stack
+		push rdi				; store bootInfo prior to calling _init
 		call _init
-		mov rdi, [_bootInfo]	; retrieve bootInfo prior to calling kernel_entry
+		pop rdi					; retrieve bootInfo prior to calling kernel_entry
 		call kernel_entry
 		call _fini
 		cli
